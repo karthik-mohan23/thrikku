4aa849ef548f84a7cd807fc029750761e32f82c5
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-12-17 19:42:30 +0530

    update metadata

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 function Navbar() {
   return (
-    <header className="bg-orange-50 text-black">
-      <nav className="w-[90%] py-5 max-w-5xl mx-auto ">
+    <header className="bg-orange-50 text-black py-5">
+      <nav className="w-[90%] max-w-5xl mx-auto">
         <Link href="/" className="text-2xl md:text-4xl font-semibold">
           Thrikku
         </Link>
